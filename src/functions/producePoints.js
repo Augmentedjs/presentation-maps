dc5423ec6bffc44de41993cd0b87d4ad5eb4be95
thedocbwarren <jedi.master@augmentedjs.com>
2019-06-23 23:33:15 -0700

    heatmap generation

--- src/functions/producePoints.js
+++ src/functions/producePoints.js
@@ -1,11 +1,17 @@
 const producePoints = (google, data) => {
   const points = [];
-  if (data && Array.isArray(data) && data.length > 0) {
+  console.debug(google);
+  if (google && data && Array.isArray(data) && data.length > 0) {
     let i = 0;
     const l = data.length;
     for (i; i < l; i++) {
-      points.push(new google.LatLng(data[i].lat, data[i].long));
+      points.push(new google.maps.LatLng(data[i].lat, data[i].long));
     }
+  } else {
+    if (!google) {
+      console.error("No Google API.");
+    }
+    console.warn("Could not produce points.");
   }
   return points;
 };
